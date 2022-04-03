94e0251ea7c545efe948994e2ff4e2437096580a
Erzhan Aldamatov <aldamatov@gmail.com>
2022-04-03 13:34:00 +1000

    Side Menu Added

--- emails/all-emails.js
+++ emails/all-emails.js
@@ -1,6 +1,4 @@
-// const primary = require('./primary');
-// const social = require('./social');
-// const promotions = require('./promotions');
+
 import { primary } from './primary.js';
 import { promotions } from './promotions.js';
 import { social } from './social.js';
